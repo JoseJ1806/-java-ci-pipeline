2c091f1075850bc52d9269d8e3678bfd87316ae6
Jose Julian <juliansolqu18@gmail.com>
2021-03-24 13:18:49 -0600

    Update calculator.java

--- src/calculator.java
+++ src/calculator.java
@@ -11,7 +11,23 @@ public class calculator {
 
         Scanner scanObject = new Scanner (System.in);
 
-        System.out
-    }
+        System.out.println("Please enter the first number: ");
+        num1 = scanObject.nextInt();
+        System.out.println("Please enter the second number: ");
+        num2 = scanObject.nextInt();
+        System.out.println("What operation? ");
+        operator = scanObject.next().charAt(0);
 
+        switch (operator){
+            case '+': answer = num1 + num2;
+                break;
+            case '-': answer = num1 - num2;
+                break;
+            case '*': answer = num1 *num2;
+                break;
+            case '/': answer = num1/num2;
+                break;
+        }
+        System.out.println(num1 + " " + operator + " " + num2 + " = " + answer);
+    }
 }
