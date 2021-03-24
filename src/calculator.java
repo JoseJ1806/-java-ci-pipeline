5fa83812023e5101cbaa6f8c450d02dc06a2bcf1
Jose Julian <juliansolqu18@gmail.com>
2021-03-24 12:59:13 -0600

    First commit

---
+++ src/calculator.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class calculator {
+
+    public static void main (String[] args){
+
+        int num1 = 0;
+        int num2 = 0;
+        char operator;
+        double answer = 0.0;
+
+        Scanner scanObject = new Scanner (System.in);
+
+        System.out
+    }
+
+}
